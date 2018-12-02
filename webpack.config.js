b85fa31ada030656d116da5a1d25da81386da110
philippepary <philippe@scil.coop>
2018-12-02 20:17:20 +0100

    Added some comments to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,55 @@
  * @author lo.pennequin@gmail.com (Daria)
  */
 
+// First we fetch dependancies
+
+// this is nodejs' path module. To handle directories
 const path = require('path');
+// This is webpack. Obviously, we need it :)
 const webpack = require('webpack');
+// Used to purge dist directory before generating new bundles so we don't get any legacy files
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+// Used to minify JS. It's called uglified because it was historically used to obfuscate…
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+// Used to minify CSS
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Creates HTML pages from templates so we can have them in the dist directory
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Used to actually create files when in dev-server mode (else they're only in RAM)
 const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = env => {
+    // set with npm run build or npm run dev
+    // Have a look at the scripts section of package.json
     const isProd = env.NODE_ENV === 'production';
     return {
         mode: env.NODE_ENV,
+        // our configuration is only for web
+        // it's regular JS, nothing related to backend JS
         target: 'web',
+        // source map helps debbuging with minified JS files
+        // we add the sources maps within our scripts
         devtool: 'inline-source-maps',
+        // entry point: this is the first file that webpack
+        // will read. Everything will get build from this one
         entry: {
             app: path.resolve(__dirname, 'src/js/index.js')
         },
+        // output: directory where generated JS will be saved
         output: {
             path: path.resolve(__dirname, 'dist/assets'),
             chunkFilename: '[name].js'
         },
+        // configuration for dev environnement with live reload
         devServer: {
             contentBase: path.resolve(__dirname, 'dist'),
             hot: true
         },
+        // all external modules, webpack only handle JS :)
         module: {
             rules: [
+                // babel: we can code in ES6 and have backward compitibility
+                // have a look at .babelrc file !
                 {
                     test: /\.js$/,
                     exclude: /node_modules/,
@@ -38,14 +60,17 @@ module.exports = env => {
                         loader: 'babel-loader',
                         options: {
                             babelrc: true,
-                            cacheDirectory: true
                         }
                     }
                 },
+                // file-loader is to ensure the presence of an image that is used
+                // in JS but is not present in HTML or CSS files
                 {
                     test: /\.(gif|png|jpe?g|svg)$/i,
                     use: 'file-loader'
                 },
+                // SASS loader: to have SASS called automatically and CSS generated
+                // Have a look at the entry point
                 {
                     test: /\.sass$/,
                     use: [
@@ -60,6 +85,7 @@ module.exports = env => {
                         'sass-loader'
                     ].filter(loader => loader !== false)
                 },
+                // Used to load straight CSS files
                 {
                     test: /\.css$/,
                     use: [
@@ -75,17 +101,22 @@ module.exports = env => {
             ]
         },
         plugins: [
-            new webpack.HotModuleReplacementPlugin(),
+            // define an __ENV__ constant for every 
             new webpack.DefinePlugin({
-                // .toString() ne marche pas ?????????
                 __ENV__: '"' + env.NODE_ENV + '"'
             }),
-            new CleanWebpackPlugin(['dist/*']),
+            // this plugin cleans dist files
+            new CleanWebpackPlugin(['dist/assets/*']),
+            // write files on the system rather than just having them in RAM
             new WriteFilePlugin(),
+            // create an minified CSS file
             new MiniCssExtractPlugin({
                 filename: '[name].css',
                 chunkFilename: '[name].0[id].css'
             }),
+            // we need this one to run dev
+            new webpack.HotModuleReplacementPlugin(),
+            // create an HTML file from template
             new HtmlWebpackPlugin({
                 template: path.join(__dirname, 'src/index.html'),
                 inject: true,
@@ -93,26 +124,12 @@ module.exports = env => {
             })
         ],
         optimization: {
-            runtimeChunk: 'single',
-            splitChunks: {
-                cacheGroups: {
-                    vendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendors',
-                        chunks: 'all'
-                    }
-                }
-            },
             minimizer: [
-                isProd &&
-                    new UglifyWebpackPlugin({
-                        uglifyOptions: {
-                            compress: {
-                                collapse_vars: false
-                            }
-                        }
-                    })
-            ].filter(plugin => plugin !== false)
+            // minimize JS on prod, not on dev
+                isProd && new UglifyWebpackPlugin()
+            ]
+            // we filter because there is nothing on dev
+            .filter(plugin => plugin !== false)
         }
     };
 };
