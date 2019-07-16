fe5ae3aeb09544699e2e1be2ea01466d3ba6df9a
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2019-07-16 22:26:18 +0200

    Give credit to original coder
    Thank you Loïc ;)

--- src/js/greet.js
+++ src/js/greet.js
@@ -1,7 +1,7 @@
 const greeter = className => {
     const el = document.createElement('div');
     el.classList.add('greet');
-    el.textContent = 'Hello PopSchool !';
+    el.textContent = 'Hello loicpennequin/Pop-School---boilerplate-Webpack!';
     document.body.appendChild(el);
 };
 
